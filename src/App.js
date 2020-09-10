b5de90b84353aa48ea5d15e9be157f967e2edcd4
Rajnil Prasad <rajnilprasad.rp@gmail.com>
2020-09-09 22:47:57 -0700

    initiall commit

--- src/App.js
+++ src/App.js
@@ -1,190 +1,48 @@
-import React, { Component } from "react";
-import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
-import Login from "./components/Login";
-import ProductList from "./components/ProductList";
-import AddProduct from "./components/AddProduct";
-import Cart from "./components/Cart";
-import data from "./Data";
-import Context from "./Context";
-import Contact from "./components/Contact";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { registerNav } from './modules/Navigation'
+import { insertToken } from './redux/action/tokenAction'
+import LoginContainer from './pages/loginsignin/LoginContainer'
+import SigninContainer from './pages/loginsignin/SigninContainer'
+import DashboardContainer from './pages/dashboard/DashboardContainer'
+import ProductOverview from './pages/productOverview/ProductOverviewContainer'
+import ShoppingBagContainer from './pages/shoppingBag/ShoppingBagContainer'
+import CheckoutContainer from './pages/checkout/checkoutContainer'
+import CheckoutSuccessContainer from './pages/checkoutSuccess/CheckoutSuccessContainer'
+import CheckoutCancel from './pages/checkoutCancel/CheckoutCancel'
 
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      cart: {},
-      products: []
-    };
-
-    this.routerRef = React.createRef();
-  }
-  login = (usn, pwd) => {
-    let user = data.users.find(u => u.username === usn && u.password === pwd);
-    if (user) {
-      this.setState({ user });
-      localStorage.setItem("user", JSON.stringify(user));
-      return true;
-    }
-    return false;
-  };
-
-  logout = e => {
-    e.preventDefault();
-    this.setState({ user: null });
-    localStorage.removeItem("user");
-  };
-
-  addProduct = (product, callback) => {
-    let products = this.state.products.slice();
-    products.push(product);
-    localStorage.setItem("products", JSON.stringify(products));
-    this.setState({ products }, () => callback && callback());
-  };
-
-  addToCart = cartItem => {
-    let cart = this.state.cart;
-    if (cart[cartItem.id]) {
-      cart[cartItem.id].amount += cartItem.amount;
-    } else {
-      cart[cartItem.id] = cartItem;
-    }
-    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
-      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
-    }
-    localStorage.setItem("cart", JSON.stringify(cart));
-    this.setState({ cart });
-  };
-
-  checkout = () => {
-    if (!this.state.user) {
-      this.routerRef.current.history.push("/login");
-      return;
-    }
-    const cart = this.state.cart;
-    const products = this.state.products.map(p => {
-      if (cart[p.name]) {
-        p.stock = p.stock - cart[p.name].amount;
-      }
-      return p;
-    });
-    this.setState({ products });
-    this.clearCart();
-  };
-
-  removeFromCart = cartItemId => {
-    let cart = this.state.cart;
-    delete cart[cartItemId];
-    localStorage.setItem("cart", JSON.stringify(cart));
-    this.setState({ cart });
-  };
-
-  clearCart = () => {
-    let cart = {};
-    localStorage.setItem("cart", JSON.stringify(cart));
-    this.setState({ cart });
-  };
-
+class App extends Component {
   componentDidMount() {
-    let products = localStorage.getItem("products");
-    let cart = localStorage.getItem("cart");
-    let user = localStorage.getItem("user");
-    products = products ? JSON.parse(products) : data.initProducts;
-    cart = cart ? JSON.parse(cart) : {};
-    user = user ? JSON.parse(user) : null;
-    this.setState({ products, user, cart });
+    this.props.insertToken()
   }
-
   render() {
     return (
-      <Context.Provider
-        value={{
-          ...this.state,
-          removeFromCart: this.removeFromCart,
-          addToCart: this.addToCart,
-          login: this.login,
-          addProduct: this.addProduct,
-          clearCart: this.clearCart,
-          checkout: this.checkout
-        }}
-      >
-        <Router ref={this.routerRef}>
-          <div className="App">
-            <nav
-              className="navbar container"
-              role="navigation"
-              aria-label="main navigation"
-            >
-              <div className="navbar-brand">
-                <b className="navbar-item is-size-4 ">Shop City</b>
-
-                <a
-                  href="/"
-                  role="button"
-                  className="navbar-burger burger"
-                  aria-label="menu"
-                  aria-expanded="false"
-                  data-target="navbarBasicExample"
-                  onClick={e => {
-                    e.preventDefault();
-                    this.setState({ showMenu: !this.state.showMenu });
-                  }}
-                >
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                </a>
-              </div>
-              <div
-                className={`navbar-menu ${
-                  this.state.showMenu ? "is-active" : ""
-                  }`}
-              >
-                <Link to="/products" className="navbar-item">
-                  Products
-                </Link>
-                {this.state.user && this.state.user.accessLevel < 1 && (
-                  <Link to="/add-product" className="navbar-item">
-                    Add Product
-                  </Link>
-                )}
-                <Link to="/cart" className="navbar-item">
-                  Cart
-                  <span
-                    className="tag is-primary"
-                    style={{ marginLeft: "5px" }}
-                  >
-                    {Object.keys(this.state.cart).length}
-                  </span>
-                </Link>
-                {!this.state.user ? (
-                  <Link to="/login" className="navbar-item">
-                    Login
-                  </Link>
-                ) : (
-                    <a href="/" className="navbar-item" onClick={this.logout}>
-                      Logout
-                    </a>
-                  )}
-
-                <Link to="/contact" className="navbar-item">
-                  Contact
-                  </Link>
-              </div>
-            </nav>
-
-            <Switch>
-              <Route exact path="/" component={ProductList} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/cart" component={Cart} />
-              <Route exact path="/add-product" component={AddProduct} />
-              <Route exact path="/products" component={ProductList} />
-              <Route exact path="/contact" component={Contact} />
-            </Switch>
-          </div>
+      <div>
+        <Router ref={registerNav}>
+          <Switch>
+            <Route path="/signin" component={SigninContainer} />
+            <Route path="/login" component={LoginContainer} />
+            <Route key="productOverview" path="/product-overview" component={ProductOverview} />,
+            {this.props.token && [
+              <Route key="ShoppingBagContainer" path="/bag" component={ShoppingBagContainer} />,
+              <Route key="Checkout" path="/checkout" component={CheckoutContainer} />,
+              <Route key="success" path="/success_page" component={CheckoutSuccessContainer} />,
+              <Route key="cancel" path="/cancel_page" component={CheckoutCancel} />,
+            ]}
+            <Route key="dashboard" path="/dashboard" component={DashboardContainer} />,
+            <Route exact path="/" component={DashboardContainer} />
+            <Redirect to='/login' />
+          </Switch>
         </Router>
-      </Context.Provider>
+      </div>
     );
   }
 }
+const mapStoreToProps = state => ({
+  token: state.token.user_token
+})
+const mapDispatchToProps = {
+  insertToken
+}
+export default connect(mapStoreToProps, mapDispatchToProps)(App);
